60152047439bb4d84f24f66d7583533c05a8404b
Gillian <gil.sampoux@gmail.com>
2017-03-01 14:32:03 +0100

    Added most viewModels

---
+++ OxfamSurveys/ViewModel/SummaryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace OxfamSurveys.ViewModel
+{
+    public class SummaryViewModel : ViewModelBase
+    {
+        private RelayCommand seeNutValCommand;
+
+        public RelayCommand SeeNutValCommand => seeNutValCommand;
+    }
+}
