ec5fdeee9ef681bdfe668728fa276177815b6463
Gillian <gil.sampoux@gmail.com>
2017-03-01 15:13:09 +0100

    Added connectionView bindings

--- OxfamSurveys/ViewModel/MenuWindowViewModel.cs
+++ OxfamSurveys/ViewModel/MenuWindowViewModel.cs
@@ -10,8 +10,36 @@ namespace OxfamSurveys.ViewModel
         private readonly RelayCommand downloadNutValCommand;
         private readonly RelayCommand createCommand;
 
+        private DateTime beginningDate;
+        private DateTime endingDate;
+        private String formsName;
+
         public ICommand DownloadNutValCommand => downloadNutValCommand;
         public ICommand CreateCommand => createCommand;
+        public DateTime BeginningDate
+        {
+            get { return beginningDate; }
+            set
+            {
+                this.beginningDate = value;
+            }
+        }
+        public DateTime EndingDate
+        {
+            get { return endingDate; }
+            set
+            {
+                this.endingDate = value;
+            }
+        }
+        public String FormsName
+        {
+            get { return formsName; }
+            set
+            {
+                this.formsName = value;
+            }
+        }
 
         public MenuWindowViewModel()
         {
