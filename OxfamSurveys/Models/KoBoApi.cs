f652c71322f9c28383248009d3589a874d728cc7
Yentl Volcke <yentl.volcke@student.howest.be>
2017-03-01 16:52:28 +0100

    Revert "merge"
    This reverts commit c602ac3b327eb07ee4b6049f7371c4b1715c9ac4

---
+++ OxfamSurveys/Models/KoBoApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OxfamSurveys.Models
+{
+    public class KoBoApi : Api
+    {
+        public bool createForm(string name, IEnumerable<Food> food)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FormData getData()
+        {
+            return new FormData(0, new List<FormLine>());
+        }
+    }
+}
---
+++ OxfamSurveys/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,40 @@
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace OxfamSurveys.ViewModel
+{
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<MenuViewModel>();
+        }
+
+        public MainViewModel MainViewModel => ServiceLocator.Current.GetInstance<MainViewModel>();
+        public MenuViewModel MenuViewModel => ServiceLocator.Current.GetInstance<MenuViewModel>();
+        public ConnectionViewModel ConnectionViewModel => ServiceLocator.Current.GetInstance<ConnectionViewModel>();
+        public SummaryViewModel SummaryViewModel => ServiceLocator.Current.GetInstance<SummaryViewModel>();
+
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
