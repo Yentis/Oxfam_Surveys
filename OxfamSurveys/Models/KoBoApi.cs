1bde94c20e714a7c59482434724ef5d51cb4a2ce
Philippe SCHOMMERS <p.schommers@student.helmo.be>
2017-03-02 15:29:53 +0100

    Basic KoBoToolbox API usage

--- OxfamSurveys/Models/KoBoApi.cs
+++ OxfamSurveys/Models/KoBoApi.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using OxfamSurveys.Models.KoBoApiRequests;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
 using System.Collections.Generic;
 
 namespace OxfamSurveys.Models
 {
     public class KoBoApi : Api
     {
+        private const string baseUrl = "https://kc.kobotoolbox.org/api/v1/";
+
+        private string username;
+        private string password;
+        private string server;
+
+        public KoBoApi(string username, string password, string server = "")
+        {
+            this.username = username;
+            this.password = password;
+            this.server = server;
+        }
+
         public bool createForm(string name, IEnumerable<Food> food)
         {
             throw new NotImplementedException();
@@ -13,6 +29,32 @@ namespace OxfamSurveys.Models
         public FormData getData()
         {
             return new FormData(0, new List<FormLine>());
+        }
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            // Always request data in JSON format
+            request.AddParameter("format", "json");
+
+            var client = new RestClient();
+            client.BaseUrl = new Uri(baseUrl);
+            client.Authenticator = new HttpBasicAuthenticator(username, password);
+            var response = client.Execute<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                throw response.ErrorException;
+            }
+
+            return response.Data;
+        }
+
+        public List<Form> GetForms()
+        {
+            var request = new RestRequest();
+            request.Resource = "forms";
+
+            return Execute<List<Form>>(request);
         }
     }
 }
