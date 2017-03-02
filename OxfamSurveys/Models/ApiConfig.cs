c9558a6332cbe2e4981c0b61c1674ff91cedc381
Philippe SCHOMMERS <p.schommers@student.helmo.be>
2017-03-02 17:54:09 +0100

    Recode API settings to make it usable

---
+++ OxfamSurveys/Models/ApiConfig.cs
@@ -0,0 +1,60 @@
+﻿using IniParser;
+using IniParser.Exceptions;
+using IniParser.Model;
+
+namespace OxfamSurveys.Models
+{
+    class ApiConfig
+    {
+        private static readonly string FILE_NAME = "api-config.ini";
+        private FileIniDataParser parser = new FileIniDataParser();
+
+        public void Set(Apis api, string username, string password, string server)
+        {
+            KeyDataCollection collection = new KeyDataCollection();
+            collection["username"] = username;
+            collection["password"] = password;
+            collection["server"] = server;
+
+            IniData config;
+            try
+            {
+                config = Get();
+            }
+            catch (ParsingException)
+            {
+                config = new IniData();
+            }
+
+            config[api.ToString()].Merge(collection);
+            parser.WriteFile(FILE_NAME, config);
+        }
+
+        public Config Get(Apis api)
+        {
+            KeyDataCollection config = Get()[api.ToString()];
+            return new Config(api, config["username"], config["password"], config["server"]);
+        }
+
+        private IniData Get()
+        {
+            return parser.ReadFile(FILE_NAME);
+        }
+
+        internal class Config
+        {
+            public Apis Api { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Server { get; set; }
+
+            public Config(Apis api, string username, string password, string server)
+            {
+                Api = api;
+                Username = username;
+                Password = password;
+                Server = server;
+            }
+        }
+    }
+}
--- OxfamSurveys/ViewModel/ConnectionViewModel.cs
+++ OxfamSurveys/ViewModel/ConnectionViewModel.cs
@@ -1,80 +1,69 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
-using System.Diagnostics;
-using System.IO;
+using IniParser.Model;
+using OxfamSurveys.Models;
+using System;
 using System.Windows.Input;
+using static OxfamSurveys.Models.ApiConfig;
 
 namespace OxfamSurveys.ViewModel
 {
     public class ConnectionViewModel : ViewModelBase
     {
-        private readonly RelayCommand saveCommand;
+        private readonly ApiConfig apiConfig = new ApiConfig();
 
-
-        public ICommand SaveCommand => saveCommand;
-
+        // TODO: Make passwords PasswordBox instead of TextBox
+        #region Public Attributes
         public string KoboLogin { get; set; }
         public string KoboPassword { get; set; }
         public string KoboUrl { get; set; }
         public string CTOLogin { get; set; }
         public string CTOPassword { get; set; }
         public string CTOUrl { get; set; }
+        #endregion
 
+        #region Commands
+        private ICommand _SaveCommand;
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return _SaveCommand ?? (
+                    _SaveCommand = new RelayCommand(() =>
+                    {
+                        apiConfig.Set(Apis.KoBoCollect, KoboLogin, KoboPassword, KoboUrl);
+                        apiConfig.Set(Apis.SurveyCTO, CTOLogin, CTOPassword, CTOUrl);
+                    })
+                );
+            }
+        }
+        #endregion
         
-
-        //Check for URL authenticity
-
         public ConnectionViewModel()
         {
-            saveCommand = new RelayCommand(SaveSettings);
-            string[] config;
-            config = RetrieveConfig();
-            KoboLogin = config[0];
-            KoboPassword = config[1];
-            KoboUrl = config[2];
-            CTOLogin = config[3];
-            CTOPassword = config[4];
-            CTOUrl = config[5];
-        }
-
-        private void SaveSettings()
-        {
-            if (CTOLogin != null && CTOPassword != null && CTOUrl != null && KoboLogin != null && KoboPassword != null && KoboUrl != null)
+            try
             {
-                string[] lines = { KoboLogin, KoboPassword, KoboUrl,CTOLogin, CTOPassword, CTOUrl };
-                string filePath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\config.txt";
-                File.WriteAllLines(filePath, lines);
+                Config koboConfig = apiConfig.Get(Apis.KoBoCollect);
+                KoboLogin = koboConfig.Username;
+                KoboPassword = koboConfig.Password;
+                KoboUrl = koboConfig.Server;
             }
-        }
-
-        private string[] RetrieveConfig()
-        {
-            string[] config = new string[6] {
-                "empty",
-                "empty",
-                "empty",
-                "empty",
-                "empty",
-                "empty"
-            };
-            string[] lines = config;
-
-            string filePath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\\config.txt";
-
-            if(!File.Exists(filePath))
+            catch (Exception)
             {
-                File.WriteAllLines(filePath, config);
-            }else
-            {
-                lines = File.ReadAllLines(filePath);
-
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    config[i] = lines[i];
-                }
+                Console.WriteLine("API config file couldn't be read");
             }
-
-            return config;
+            
+            try
+            {
+                Config CTOConfig = apiConfig.Get(Apis.SurveyCTO);
+                CTOLogin = CTOConfig.Username;
+                CTOPassword = CTOConfig.Password;
+                CTOUrl = CTOConfig.Server;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("API config file couldn't be read");
+            }
         }
     }
 }
