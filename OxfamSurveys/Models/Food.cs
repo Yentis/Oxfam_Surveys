386e9a6b534a133869167709c67e9af244653eff
Gillian <gil.sampoux@gmail.com>
2017-03-16 10:30:51 +0100

    Deleted useless propriety in Food

--- OxfamSurveys/Models/Food.cs
+++ OxfamSurveys/Models/Food.cs
@@ -4,13 +4,11 @@
     {
         public string Name { get; }
         public string Type { get; }
-        public int PeopleNbr { get; set; }
 
         public Food(string name, string type)
         {
             Name = name;
             Type = type;
-            PeopleNbr = 0;
         }
     }
 }
